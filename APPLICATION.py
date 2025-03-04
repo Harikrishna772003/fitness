06adc11fc74acea5c44c802c6fae460368cc9e40
Harikrishna772003 <151329276+Harikrishna772003@users.noreply.github.com>
2025-03-04 13:52:15 +0530

    Update APPLICATION.py

--- APPLICATION.py
+++ APPLICATION.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
